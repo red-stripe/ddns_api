c06befcb9d7f36cf51347a0a9a219f5fda88b0dd
Craig H <red-stripe@users.noreply.github.com>
2020-12-18 14:10:30 +0900

    Add: Production setup files

--- ddns_django/settings.py
+++ ddns_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lcu70%$$qem2h!akfc6#)44d2pw+urmh^mdj#j84hq5=73pgjr'
+#SECRET_KEY = 'lcu70%$$qem2h!akfc6#)44d2pw+urmh^mdj#j84hq5=73pgjr'
 
-#with open('/etc/secret_key.txt') as f:
-#    SECRET_KEY = f.read().strip()
+with open('/etc/secret_key.txt') as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
@@ -133,3 +135,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
 }
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
